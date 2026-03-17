15da65a42b1635e1685e6a581a207cb4361aa5fe
Hazel Cunuder <hazel.claire.lee@gmail.com>
2026-03-17 15:49:07 +0100

    feat: removed unnamed column

--- src/data-pipeline/extract/txt_data_gouv.py
+++ src/data-pipeline/extract/txt_data_gouv.py
@@ -7,7 +7,7 @@ df = pd.read_csv(BASE_DIR / 'flatfiles' / 'communes-france-2025.csv')
 
 pd.options.display.max_columns = None
 
-df = df.drop(df.columns[[4,5,8,9,10,11,14,15,16,17,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46]], axis=1)
+df = df.drop(df.columns[[0,4,5,8,9,10,11,14,15,16,17,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46]], axis=1)
 
 print(df.columns)
 

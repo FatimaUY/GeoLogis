4666b7ac175b0e11ba2e23d4a974853827451655
Azzi <locked.hrt@hotmail.fr>
2026-03-20 09:51:07 +0100

    feature: creation frontend

--- src/django-app/GeoLogis/views.py
+++ src/django-app/GeoLogis/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
+def liste_articles(request):
+    articles = Article.objects.all()
+    context = {'articles': articles}
+    return render(request, 'GeoLogis/liste_articles.html', context) 
 # Create your views here.
--- src/django-app/config/urls.py
+++ src/django-app/config/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from GeoLogis.views import liste_articles
 
 urlpatterns = [
+    path('', liste_articles, name='home'),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/market/', include('market.urls')),
     path('api/predictions/', include('predictions.urls')),
+    path('api/geologis/', include('GeoLogis.urls')),
 ]

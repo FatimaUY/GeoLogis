2b1df38c26edddbe1a636b564065100832010182
Azzi <locked.hrt@hotmail.fr>
2026-03-24 16:32:44 +0100

    feature frontend general

--- src/django-app/GeoLogis/views.py
+++ src/django-app/GeoLogis/views.py
@@ -1,9 +1,14 @@
 from django.views.generic import ListView
 from .models import Article
+from django.shortcuts import render
 
 class ListeArticlesView(ListView):
     model = Article
     template_name = 'GeoLogis/acceuil.html'
     context_object_name = 'articles'
+
+def info_view(request):
+    return render(request, 'info.html')
+
     
  

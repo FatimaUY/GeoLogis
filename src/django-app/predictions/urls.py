e84f1b9b537e77b061489816cc0953a655a1ea22
Azzi <locked.hrt@hotmail.fr>
2026-03-25 13:53:28 +0100

    feature: map in prediction page

--- src/django-app/predictions/urls.py
+++ src/django-app/predictions/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import predict, get_predictions
 
 urlpatterns = [
-    path("predict/", predict),
-    path("", get_predictions),
+    # Laisse le chemin vide '' car il sera complété par le fichier principal
+    path('', predict, name='prediction'), 
+    
+    # Route pour ton API d'historique si besoin
+    path('history/', get_predictions, name='prediction_history'),
 ]
